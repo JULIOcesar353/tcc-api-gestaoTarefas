44f0dbf799397cd3fc53146e0315581ee4f8d836
dev_Julio <j.duque.junior@gmail.com>
2025-12-01 22:30:23 -0300

    imagens finalizadas

--- src/routes/ryan.js
+++ src/routes/ryan.js
@@ -4,13 +4,18 @@ const router = express.Router();
 const CargoPermissoesController = require('../controllers/CargoPermissoes');
 const TarefasFotosController = require('../controllers/TarefaFotos');
 
+const uploadImage = require('../middleware/uploadHelper');
+
+//Middleware configurado para a pasta  'tarefas'
+const uploadTarefas = uploadImage('tarefas');
+
 router.get('/cargoPermissoes', CargoPermissoesController.listarCargoPermissoes);
 router.post('/cargoPermissoes', CargoPermissoesController.cadastrarCargoPermissoes);
 router.patch('/CargoPermissoes/:crg_id/:prm_id', CargoPermissoesController.editarCargoPermissoes);
 router.delete('/cargoPermissoes/:crg_id/:prm_id', CargoPermissoesController.apagarCargoPermissoes);
 
 router.get('/tarefaFotos', TarefasFotosController.listarTarefaFotos);
-router.post('/tarefaFotos', TarefasFotosController.cadastrarFotosTarefas);
+router.post('/tarefaFotos', uploadTarefas.single('img'), TarefasFotosController.cadastrarFotosTarefas);
 router.patch('/tarefaFotos/:id', TarefasFotosController.editartarefaFotos);
 router.delete('/tarefaFotos/:id', TarefasFotosController.excluirtarefaFotos);  
 
