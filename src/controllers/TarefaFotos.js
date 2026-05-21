05b4934ddc2fb2e41d51c24dae206a4d6acf5239
RHYAN AUGUSTO RODRIGUES <rhyan.rodrigues01>
2026-05-20 22:47:58 -0300

    20/05 Adicionei opção de enviar foto qnd termianr a tarefa na tela funcionários

--- src/controllers/TarefaFotos.js
+++ src/controllers/TarefaFotos.js
@@ -1,86 +1,97 @@
-const db = require ('../dataBase/connection')
-const { gerarUrl } = require('../utils/gerarUrl');
+const db = require("../dataBase/connection");
+const { gerarUrl } = require("../utils/gerarUrl");
 
 module.exports = {
-    async listarTarefaFotos (request, response) {
-      try{ 
-
-        const sql = `
+  async listarTarefaFotos(request, response) {
+    try {
+      const sql = `
         SELECT 
         fot_id, fot_tarefa_id, fot_nome, fot_descricao, fot_data_envio 
         FROM TAREFA_FOTOS;
         `;
 
-        const [tarefafotos] = await db.query(sql);
-        const nItens =  tarefafotos.length;
+      const [tarefafotos] = await db.query(sql);
+      const nItens = tarefafotos.length;
 
-        const dados = tarefafotos.map(tarefa => ({
-            ...tarefa,
-            fot_nome: gerarUrl(tarefa.fot_nome, 'tarefas', 'sem.jpg')
-        }));
+      const dados = tarefafotos.map((tarefa) => ({
+        ...tarefa,
+        fot_nome: gerarUrl(tarefa.fot_nome, "tarefas", "sem.jpg"),
+      }));
 
-        return response.status(200).json(
-            {
-            sucesso: true,
-            mensagem: 'Foto de Tarefas obtida com sucesso',
-            nItens,
-            dados
+      return response.status(200).json({
+        sucesso: true,
+        mensagem: "Foto de Tarefas obtida com sucesso",
+        nItens,
+        dados,
+      });
+    } catch (error) {
+      return response.status(500).json({
+        sucesso: false,
+        mensagem: `Erro ao listar Foto das Tarefas: ${error.message}`,
+        dados: null,
+      });
+    }
+  },
+  // ------------ Cadastrar Fotos de Tarefas -------------
+  async cadastrarFotosTarefas(request, response) {
+    try {
+      const { tarefa, descricao } = request.body;
+      const imagem = request.file;
+
+      if (!tarefa) {
+        return response.status(400).json({
+          sucesso: false,
+          mensagem: "ID da tarefa é obrigatório.",
+          dados: null,
         });
-    }  catch (error) {
-        return response.status(500).json(
-            {
-                sucesso: false,
-                mensagem: `Erro ao listar Foto das Tarefas: ${error.message}`,
-                dados: null
-            }
-        );
+      }
+
+      if (!imagem) {
+        return response.status(400).json({
+          sucesso: false,
+          mensagem: "Imagem da tarefa é obrigatória.",
+          dados: null,
+        });
+      }
+
+      const sql = `
+      INSERT INTO TAREFA_FOTOS 
+        (fot_tarefa_id, fot_nome, fot_descricao, fot_data_envio)
+      VALUES
+        (?, ?, ?, NOW());
+    `;
+
+      const values = [tarefa, imagem.filename, descricao];
+
+      const [result] = await db.query(sql, values);
+
+      return response.status(200).json({
+        sucesso: true,
+        mensagem: "Cadastro de Foto das Tarefas efetuada com sucesso",
+        dados: {
+          id: result.insertId,
+          tarefa,
+          nome: imagem.filename,
+          descricao,
+        },
+      });
+    } catch (error) {
+      return response.status(500).json({
+        sucesso: false,
+        mensagem: "Erro ao Cadastrar Foto das Tarefas.",
+        dados: error.message,
+      });
     }
-    },
-// ------------ Cadastrar Fotos de Tarefas -------------
-    async cadastrarFotosTarefas (request, response) {
-      try{ 
-        const {tarefa, nome, descricao} = request.body;
-        const imagem = request.file;
+  },
+  
+  // ------------ Editar Foto das Tarefas -------------
+  async editartarefaFotos(request, response) {
+    try {
+      const { nome, descricao, data_envio } = request.body;
 
-        const sql = `INSERT INTO TAREFA_FOTOS 
-            (fot_tarefa_id, fot_nome, fot_descricao, fot_data_envio)
-        VALUES
-            (?, ?, ?, NOW());`;
+      const { id } = request.params;
 
-        const values = [tarefa, imagem.filename, descricao];
-
-        const [result] = await db.query(sql, values);
-
-        const dados = {
-            id: result.insertId,
-            descricao   
-        };
-        return response.status(200).json(
-            {
-            sucesso: true,
-            mensagem: 'Cadastro de Foto das Tarefas efetuada com sucesso',
-            dados: dados
-        }
-    );
-    }  catch (error) {
-        return response.status(500).json(
-            {
-                sucesso: false,
-                mensagem: `Erro ao Cadastrar Foto das Tarefas.`,
-                dados: error.message
-            }
-        );
-    }
-    },
-// ------------ Editar Foto das Tarefas -------------
-    async editartarefaFotos (request, response) {
-      try{ 
-
-         const {nome, descricao, data_envio} = request.body;
-
-            const { id } = request.params;
-
-            const sql = `
+      const sql = `
                 UPDATE tarefa_Fotos SET
                     fot_nome = ?,
                     fot_descricao = ?,
@@ -89,79 +100,68 @@ module.exports = {
                     fot_id = ?
             `;
 
-            const values = [ nome, descricao, data_envio, id];
+      const values = [nome, descricao, data_envio, id];
 
-            const [result] = await db.query(sql, values);
+      const [result] = await db.query(sql, values);
 
-            if (result.affectedRows === 0) {
-                return response.status(404).json({
-                    sucesso: false,
-                    mensagem: `Foto da tarefa ${id} não encontrado!`,
-                    dados: null
-                });
-            }
+      if (result.affectedRows === 0) {
+        return response.status(404).json({
+          sucesso: false,
+          mensagem: `Foto da tarefa ${id} não encontrado!`,
+          dados: null,
+        });
+      }
 
-            const dados = {
-                id,
-                nome,
-                descricao,
-                data_envio
-            };
+      const dados = {
+        id,
+        nome,
+        descricao,
+        data_envio,
+      };
 
-
-        return response.status(200).json(
-            {
-            sucesso: true,
-            mensagem: 'Atualização de Fotos das Tarefas efetuada com sucesso',
-            dados: null
-        }
-    );
-    }  catch (error) {
-        return response.status(500).json(
-            {
-                sucesso: false,
-                mensagem: `Erro ao Editar Foto das Tarefas: ${error.message}`,
-                dados: null
-            }
-        );
+      return response.status(200).json({
+        sucesso: true,
+        mensagem: "Atualização de Fotos das Tarefas efetuada com sucesso",
+        dados: null,
+      });
+    } catch (error) {
+      return response.status(500).json({
+        sucesso: false,
+        mensagem: `Erro ao Editar Foto das Tarefas: ${error.message}`,
+        dados: null,
+      });
     }
-    },
-// ------------ Excluir Foto das T arefas -------------
-    async excluirtarefaFotos (request, response) {
-      try{ 
-            const { id } = request.params;
+  },
+  // ------------ Excluir Foto das T arefas -------------
+  async excluirtarefaFotos(request, response) {
+    try {
+      const { id } = request.params;
 
-            const sql = `
+      const sql = `
                 DELETE FROM tarefa_fotos
                 WHERE fot_id = ?
             `;
 
-            const values = [id];
-            const [result] = await db.query(sql, [values]);
+      const values = [id];
+      const [result] = await db.query(sql, [values]);
 
-            if (result.affectedRows === 0) {
-            return response.status(404).json({
-                sucesso: false,
-                mensagem: `Foto tarefa não encontrada!`
-                
-            });
-        }
-        return response.status(200).json(
-            {
-            sucesso: true,
-            mensagem: 'Exclusão de Fotos das tarefas efetuada com sucesso',
-            dados: null
-        }
-    );
-    }  catch (error) {
-        return response.status(500).json(
-            {
-                sucesso: false,
-                mensagem: `Erro ao Excluir Foto das Tarefas: ${error.message}`,
-                dados: null
-            }
-        );
+      if (result.affectedRows === 0) {
+        return response.status(404).json({
+          sucesso: false,
+          mensagem: `Foto tarefa não encontrada!`,
+        });
+      }
+      return response.status(200).json({
+        sucesso: true,
+        mensagem: "Exclusão de Fotos das tarefas efetuada com sucesso",
+        dados: null,
+      });
+    } catch (error) {
+      return response.status(500).json({
+        sucesso: false,
+        mensagem: `Erro ao Excluir Foto das Tarefas: ${error.message}`,
+        dados: null,
+      });
     }
-    },
-}
-
+  },
+};
